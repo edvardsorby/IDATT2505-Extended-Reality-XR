6fa2ebe7259d5f683b406aa5408ae2201ecb126c
Edvard Sørby <edvarso@stud.ntnu.no>
2023-11-16 11:26:00 +0100

    Added controls page

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,6 +4,9 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
 
+    public GameObject menuPage;
+    public GameObject controlsPage;
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -25,5 +28,17 @@ public class MenuScript : MonoBehaviour
     {
         Settings.treadmillMode = false;
         ChangeScene("Game");
+    }
+
+    public void ShowControls()
+    {
+        menuPage.SetActive(false);
+        controlsPage.SetActive(true);
+    }
+
+    public void HideControls()
+    {
+        menuPage.SetActive(true);
+        controlsPage.SetActive(false);
     }
 }
