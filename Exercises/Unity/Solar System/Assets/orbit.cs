9a5e781e48e1bcfcb9eb427a824f259a70b3b090
Edvard Sørby <edvarso@stud.ntnu.no>
2025-08-05 21:48:35 +0200

    Add Unity exercise

---
+++ Exercises/Unity/Solar System/Assets/orbit.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbit : MonoBehaviour
+{
+    public Transform center;
+
+    public float radius;
+    public float radiusSpeed;
+    public float rotationSpeed;
+
+    private Vector3 axis;
+    private Vector3 desiredPosition;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+        axis = Vector3.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+        transform.position = (transform.position - center.position).normalized * radius + center.position;
+        transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+    }
+}
